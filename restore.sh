89b29c08261500ba6be8ce1c60052c3a992c9785
DTD369 <149343776+DTD369@users.noreply.github.com>
2026-01-16 11:35:24 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1532,7 +1532,7 @@ device_get_info() {
             device_use_build="13G37"
         ;;
         iPad3,4 | iPhone5,[34] )
-            device_use_vers="10.3.4"
+            device_use_vers="10.3.3"
             device_use_build="14G60"
         ;;
         iPad3,[56] | iPhone5,[12] )
